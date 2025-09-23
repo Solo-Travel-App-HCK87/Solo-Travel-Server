f11577d219d3e4ea5ea6fb7b52699a356ddbf526
syihabb74 <syihabb74@gmail.com>
2025-09-23 16:42:14 +0700

    chore: add transaction table

---
+++ migrations/20250923092450-create-transaction.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references : {
+          model : 'Users',
+          key : 'id'
+        }
+      },
+      TravelPackageId: {
+        type: Sequelize.INTEGER,
+        references : {
+          model : 'TravelPackages',
+          key : 'id'
+        }
+      },
+      quantity: {
+        type: Sequelize.INTEGER
+      },
+      total_price: {
+        type: Sequelize.INTEGER
+      },
+      booking_date: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Transactions');
+  }
+};
