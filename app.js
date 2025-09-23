a90cc229338bc9efc6daa1d7177ecbfe4e512b3a
syihabb74 <syihabb74@gmail.com>
2025-09-23 17:05:27 +0700

    chore: add package controller

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const cors = require('cors')
 const express = require('express')
-const UserController = require('./controllers/UserController')
-const app = express()
-const port = 3000
+const UserController = require('./controllers/UserController');
+const PackageController = require('./controllers/PackageController');
+const app = express();
+const port = 3000;
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.post('/register', UserController.register)
-app.post('/login', UserController.login)
+app.post('/login', UserController.login);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
+
+
+
+
+
+
+app.get('/packages', PackageController.getPackageList)
+app.get('/packages/:id', PackageController.getPackageById)
