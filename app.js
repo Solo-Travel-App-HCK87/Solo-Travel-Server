a0b0d79bed6f9f815c20873d9018490beb69f21e
Joshua <joshuajulyus27@gmail.com>
2025-09-23 15:43:00 +0700

    chore: bcrypt/jwt/register/login/dotenv

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+console.log({ env: process.env.NODE_ENV });
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const cors = require('cors')
 const express = require('express')
+const UserController = require('./controllers/UserController')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/register', UserController.register)
+app.post('/login', UserController.login)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
