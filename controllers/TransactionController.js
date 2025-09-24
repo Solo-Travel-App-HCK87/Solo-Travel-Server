360ef1825eb9a93130314f6fdb800272eedb8ac5
Gerry0303 <imanuelgerry0303@gmail.com>
2025-09-24 22:51:30 +0700

    refactor: clean up code formatting and improve readability

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -1,38 +1,43 @@
-const {TravelPackage, Transaction} = require('../models');
+const { TravelPackage, Transaction } = require('../models');
 
 class TransactionController {
+  static async createTransaction(req, res, next) {
+    try {
+      const { packagesId } = req.params;
+      const isPackageExist = await TravelPackage.findByPk(+packagesId);
+      console.log(
+        'maskfjaksdfhakljsdfhajkdsfhaljksdfhkajdhsfkjashdfkjahdfjkahdfjhasdfhasdkfhadskfhakdjsfhalksdf'
+      );
+      if (!isPackageExist) return next({ name: 'NotFound', message: 'Package not found' });
+      if (!isPackageExist.available_slots)
+        return next({ name: 'BadRequest', message: 'Stocks book is empty' });
+      const isPackageBought = await Transaction.findOne({
+        where: {
+          TravelPackageId: isPackageExist.id,
+          UserId: req.user.id,
+        },
+      });
+      console.log(isPackageBought);
+      if (isPackageBought)
+        return next({
+          name: 'BadRequest',
+          message: 'You already buy this package please choose another Travel Package',
+        });
+      const numberOrder = await Transaction.count();
+      await isPackageExist.decrement('available_slots');
+      console.log(isPackageExist.id, req.user.id, numberOrder + 1);
+      const newOrder = await Transaction.create({
+        TravelPackageId: isPackageExist.id,
+        UserId: req.user.id,
+        OrderId: numberOrder + 1,
+      });
+      res.status(201).json(newOrder);
+    } catch (error) {
+      console.log(error);
 
-    static async createTransaction (req,res,next) {
-
-        try {
-
-            const {packagesId} = req.params;
-            const isPackageExist = await TravelPackage.findByPk(+packagesId);
-            console.log("maskfjaksdfhakljsdfhajkdsfhaljksdfhkajdhsfkjashdfkjahdfjkahdfjhasdfhasdkfhadskfhakdjsfhalksdf")
-            if (!isPackageExist) return next({ name: 'NotFound', message: 'Package not found' });
-            if (!isPackageExist.available_slots) return next({name : 'BadRequest', message : 'Stocks book is empty'});
-            const isPackageBought = await Transaction.findOne({where : {
-                TravelPackageId : isPackageExist.id
-            }});
-            console.log(isPackageBought)
-            if (isPackageBought) return next({name : 'BadRequest', message : 'You already buy this package please choose another Travel Package'});
-            const numberOrder = await Transaction.count();
-            await isPackageExist.decrement('available_slots');
-            console.log(isPackageExist.id, req.user.id, numberOrder + 1);
-            const newOrder = await Transaction.create({TravelPackageId : isPackageExist.id, UserId : req.user.id, OrderId : numberOrder + 1 });
-            res.status(201).json(newOrder);
-
-        } catch (error) {
-
-            console.log(error)
-            
-            next(error)
-
-        }
-
+      next(error);
     }
-
+  }
 }
 
-
-module.exports = { TransactionController }
+module.exports = { TransactionController };
--- seeders/20250923102443-seed-package.js
+++ seeders/20250923102443-seed-package.js
@@ -1,9 +1,9 @@
 'use strict';
-const fs = require("fs").promises;
+const fs = require('fs').promises;
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -12,33 +12,34 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
 
-    const travelPackages = JSON.parse(await fs.readFile('./data/travelPackages.json', 'utf-8'))
-                          .map((travel) => {
-                            travel.categories = JSON.stringify(travel.categories);
-    travel.inclusions = JSON.stringify(travel.inclusions);
-    travel.itinerary = JSON.stringify(travel.itinerary);
-    travel.preparation_docs = JSON.stringify(travel.preparation_docs);
-    travel.preparation_clothing = JSON.stringify(travel.preparation_clothing);
-    travel.preparation_essentials = JSON.stringify(travel.preparation_essentials);
-    travel.preparation_electronics = JSON.stringify(travel.preparation_electronics);
-    travel.createdAt = new Date();
-    travel.updatedAt = new Date();
-    travel.highlights = JSON.stringify(travel.highlights)
-    return travel;
-                          });
+    const travelPackages = JSON.parse(await fs.readFile('./data/travelpackages.json', 'utf-8')).map(
+      (travel) => {
+        travel.categories = JSON.stringify(travel.categories);
+        travel.inclusions = JSON.stringify(travel.inclusions);
+        travel.itinerary = JSON.stringify(travel.itinerary);
+        travel.preparation_docs = JSON.stringify(travel.preparation_docs);
+        travel.preparation_clothing = JSON.stringify(travel.preparation_clothing);
+        travel.preparation_essentials = JSON.stringify(travel.preparation_essentials);
+        travel.preparation_electronics = JSON.stringify(travel.preparation_electronics);
+        travel.createdAt = new Date();
+        travel.updatedAt = new Date();
+        travel.highlights = JSON.stringify(travel.highlights);
+        return travel;
+      }
+    );
 
     await queryInterface.bulkInsert('TravelPackages', travelPackages);
-
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+    await queryInterface.bulkDelete('TravelPackages', null, {});
+  },
 };
