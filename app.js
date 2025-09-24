d36de0ea5c1bbe967a5368813237f9cd0ec06293
Gerry0303 <imanuelgerry0303@gmail.com>
2025-09-24 13:39:00 +0700

    feat: integrate Socket.IO for real-time communication and update dependencies

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log({ env: process.env.NODE_ENV });
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+const { User } = require('./models');
 const cors = require('cors');
 const multer = require('multer');
 const express = require('express');
@@ -13,10 +14,43 @@ const app = express();
 const port = 3000;
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const { verifyToken } = require('./helpers/jwt');
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ['http://localhost:5173'],
+  },
+});
+
+io.on('connection', async (socket) => {
+  const access_token = socket.handshake.auth.access_token;
+  console.log(access_token);
+  if (!access_token) {
+    console.log('No access token provided');
+    return;
+  }
+  const payload = verifyToken(access_token);
+  const user = await User.findByPk(payload.id);
+
+  socket.on('chat_message', (msg) => {
+    console.log(msg);
+
+    const messageData = {
+      message: msg.message,
+      sender: user,
+      access_token: access_token,
+      createdAt: new Date(),
+    };
+    console.log(messageData);
+
+    io.emit('message_info', messageData);
+  });
+});
 
 app.post('/register', UserController.register);
 app.post('/login', UserController.login);
@@ -30,9 +64,9 @@ app.patch(
 
 app.use(errorHandling);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
 app.get('/packages', PackageController.getPackageList);
 app.get('/packages/:id', PackageController.getPackageById);
+
+httpServer.listen(port, () => {
+  console.log(`Server running at: http://localhost:${port}`);
+});
